133b95e1c01bc9de9b3e929d03faadc80930715c
MarkDantes <amirkapkaev@yandex.ru>
2021-09-30 00:52:01 +0300

    Draw a triangle

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,26 +29,69 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(636, 454);
+            this.button1.Location = new System.Drawing.Point(737, 468);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 53);
+            this.button1.Size = new System.Drawing.Size(94, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "Draw ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(350, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(350, 476);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(477, 476);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(610, 476);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(872, 539);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +99,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,16 +12,41 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        bool draw = false; 
+        bool draw = false;
+        PointF pnt1, pnt2, pnt3; 
+        
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            draw = true; 
+            draw = true;
+            Random rnd = new Random();
+            Bitmap bt = new Bitmap(pictureBox1.Width - 1, pictureBox1.Height - 1);
+            Graphics g = Graphics.FromImage(bt);
+
+             int x1 = rnd.Next(100, 300);
+             int y1 = rnd.Next(100, 250);
+         
+             int y2 = rnd.Next(100, 250);
+             
+
+             pnt1 = new PointF(x1, y1-50);
+             pnt2 = new PointF(x1+50, y2+50);
+             pnt3 = new PointF(x1-50, y2+50);
+            PointF[] points = { pnt1, pnt2, pnt3 };
+            
+            g.DrawPolygon(Pens.Black, points);
+
+            pictureBox1.Image = bt;
+            
+            
 
         }
+
+        
     }
 }
