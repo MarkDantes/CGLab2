c863036764656006dd5a7e476fe0328f54ba1788
MarkDantes <amirkapkaev@yandex.ru>
2021-10-01 00:07:07 +0300

    Collab
    Co-Authored-By: Dmitry Galushko <44999145+Popiron@users.noreply.github.com>

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,14 +33,16 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBoxLineDrawer = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(737, 468);
+            this.button1.Location = new System.Drawing.Point(493, 304);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 39);
+            this.button1.Size = new System.Drawing.Size(73, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Draw ";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +51,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictureBox1.Location = new System.Drawing.Point(350, 42);
+            this.pictureBox1.Location = new System.Drawing.Point(233, 27);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(333, 256);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -66,9 +69,10 @@
             "Blue",
             "Indigo",
             "Violet"});
-            this.comboBox1.Location = new System.Drawing.Point(350, 476);
+            this.comboBox1.Location = new System.Drawing.Point(233, 305);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(82, 24);
             this.comboBox1.TabIndex = 5;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
@@ -83,9 +87,10 @@
             "Blue",
             "Indigo",
             "Violet"});
-            this.comboBox2.Location = new System.Drawing.Point(477, 476);
+            this.comboBox2.Location = new System.Drawing.Point(318, 305);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.Size = new System.Drawing.Size(82, 24);
             this.comboBox2.TabIndex = 6;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
             // 
@@ -100,26 +105,43 @@
             "Blue",
             "Indigo",
             "Violet"});
-            this.comboBox3.Location = new System.Drawing.Point(610, 476);
+            this.comboBox3.Location = new System.Drawing.Point(407, 305);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.Size = new System.Drawing.Size(82, 24);
             this.comboBox3.TabIndex = 7;
             this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox3_SelectedIndexChanged);
+            // 
+            // checkBoxLineDrawer
+            // 
+            this.checkBoxLineDrawer.AutoSize = true;
+            this.checkBoxLineDrawer.Location = new System.Drawing.Point(8, 8);
+            this.checkBoxLineDrawer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBoxLineDrawer.Name = "checkBoxLineDrawer";
+            this.checkBoxLineDrawer.Size = new System.Drawing.Size(147, 21);
+            this.checkBoxLineDrawer.TabIndex = 0;
+            this.checkBoxLineDrawer.Text = "Enable line drawer";
+            this.checkBoxLineDrawer.UseVisualStyleBackColor = true;
+            this.checkBoxLineDrawer.CheckedChanged += new System.EventHandler(this.onCheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 539);
+            this.ClientSize = new System.Drawing.Size(603, 362);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBoxLineDrawer);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.onMouseClick);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -130,6 +152,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBoxLineDrawer;
     }
 }
 
