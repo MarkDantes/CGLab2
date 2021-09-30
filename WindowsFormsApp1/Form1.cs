02775a87c960ffbf50fe3127e682ef1d472bad62
MarkDantes <amirkapkaev@yandex.ru>
2021-09-30 23:00:03 +0300

    nothing important

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,10 +12,11 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        bool draw = false;
-        PointF pnt1, pnt2, pnt3;
+        
+        PointF a, b, c;
         Color c1, c2, c3;
         
+
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBox1.SelectedIndex == 0)
@@ -133,33 +134,58 @@ namespace WindowsFormsApp1
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            draw = true;
+            pictureBox1.Refresh();
             Random rnd = new Random();
-            Bitmap bt = new Bitmap(pictureBox1.Width - 1, pictureBox1.Height - 1);
-            Graphics g = Graphics.FromImage(bt);
+            // Bitmap bt = new Bitmap(pictureBox1.Width - 1, pictureBox1.Height - 1);
+            //Graphics g = Graphics.FromImage(bt);
 
-             int x1 = rnd.Next(100, 300);
-             int y1 = rnd.Next(100, 250);
-         
-             int y2 = rnd.Next(100, 250);
-             
+            int x1 = rnd.Next(100, 300);
+            int y1 = rnd.Next(100, 250);
 
-             pnt1 = new PointF(x1, y1-50);
-             pnt2 = new PointF(x1+50, y2+50);
-             pnt3 = new PointF(x1-50, y2+50);
-            PointF[] points = { pnt1, pnt2, pnt3 };
+            int y2 = rnd.Next(100, 250);
+
+
+            a = new PointF(x1, y1 - 50);
+            b = new PointF(x1 + 50, y2 + 50);
+            c = new PointF(x1 - 50, y2 + 50);
+
+            PointF[] points = { a, b, c };
+
+            //g.DrawPolygon(Pens.Black, points);
+
+            Gradient();
             
-            g.DrawPolygon(Pens.Black, points);
+            //pictureBox1.Image = bt;
 
-            pictureBox1.Image = bt;
-            
         }
 
-        
+        //Находим описывающий прямоугольник
+        public (PointF,PointF) R()
+        {
+            var minP = new PointF(Math.Min(a.X, Math.Min(b.X, c.X)), Math.Min(a.Y, Math.Min(b.Y, c.Y)));
+            var maxP = new PointF(Math.Max(a.X, Math.Max(b.X, c.X)), Math.Max(a.Y, Math.Max(b.Y, c.Y)));
+            return (minP, maxP);
+        }
+        //Проверяем находится ли точка внутри треугольника
+        public bool InTriangle(float x, float y)
+        {
+            float aSide = (a.Y - b.Y) * x + (b.X - a.X) * y + (a.X * b.Y - b.X * a.Y);
+            float bSide = (b.Y - c.Y) * x + (c.X - b.X) * y + (b.X * c.Y - c.X * b.Y);
+            float cSide = (c.Y - a.Y) * x + (a.X - c.X) * y + (c.X * a.Y - a.X * c.Y);
+            return (aSide >= 0 && bSide >= 0 && cSide >= 0) || (aSide < 0 && bSide < 0 && cSide < 0);
+        }
 
+       
+
+        
+        //Процедура окраски в градиентный цвет
         public void Gradient()
         {
+            Bitmap image = new Bitmap(pictureBox1.Width - 1, pictureBox1.Height - 1);
+
             
+
+                    pictureBox1.Image = image;
         }
 
 
